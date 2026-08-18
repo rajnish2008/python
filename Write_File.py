0214305c5850a402cdb9199b5839bc38993db8cf
rajnish2008 <74342012+rajnish2008@users.noreply.github.com>
2026-08-18 23:12:18 +0530

    Create Write_File.py to write student grades

---
+++ Write_File.py
@@ -0,0 +1,10 @@
+file = open("students.txt", "w")
+
+file.write("John - Grade A\n")
+file.write("Alice - Grade B\n")
+file.write("Bob - Grade C\n")
+file.write("Emma - Grade D\n")
+
+file.close()
+
+print("Content written to the file successfully.")
